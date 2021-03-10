21d127fa727fd53724593cee91967b04e964e3b8
faceoffwOw <get_right101@mail.ru>
2021-03-10 14:17:23 +0300

    Update Data.cs

--- autoinstall_wpf/Data.cs
+++ autoinstall_wpf/Data.cs
@@ -17,14 +17,14 @@ namespace autoinstall_wpf
         {
             List<Data> result = new List<Data>();
 
-            result.Add(new Data { Site = "mistwo", User = "bkotwo", Password = "bkotwo", });
-            result.Add(new Data { Site = "misone", User = "bkoone", Password = "bkoone", });
-            result.Add(new Data { Site = "bko-dis", User = "bkoces", Password = "bkoces", });
-            result.Add(new Data { Site = "serv1osm", User = "bkoosm", Password = "bkoosm", });
-            result.Add(new Data { Site = "bko-mis", User = "bkoprop", Password = "bkoprop", });
-            result.Add(new Data { Site = "misdou", User = "bkodou", Password = "bkodou", });
-            result.Add(new Data { Site = "webcsp", User = "bkocsp", Password = "bkocsp", });
-            result.Add(new Data { Site = "webdou2", User = "bkodou2", Password = "bkodou2", });
+            result.Add(new Data { Site = "site", User = "username", Password = "password", });
+            result.Add(new Data { Site = "site", User = "username", Password = "password", });
+            result.Add(new Data { Site = "site", User = "username", Password = "password", });
+            result.Add(new Data { Site = "site", User = "username", Password = "password", });
+            result.Add(new Data { Site = "site", User = "username", Password = "password", });
+            result.Add(new Data { Site = "site", User = "username", Password = "password", });
+            result.Add(new Data { Site = "site", User = "username", Password = "password", });
+            result.Add(new Data { Site = "site", User = "username", Password = "password", });
 
             return result;
         }
